ecef7d3564de039033c27640f9205d8fdac8ad48
aboutjm <meimei0730@163.com>
2014-08-20 22:54:40 +0800

    高合金钢

--- clauncher/test.js
+++ clauncher/test.js
@@ -13,7 +13,9 @@ var test1 = require('./test.js');
 
 
 
+
     casper.log('this is a debug message', 'debug');
     casper.log('and an informative one', 'info');
     casper.log('and a warning', 'warning');
     casper.log('and an error', 'error');
+    // pp
