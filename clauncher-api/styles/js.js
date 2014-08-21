9225e97396aa412ce6950e233b6162bdf4b305c3
aboutjm <meimei0730@163.com>
2014-08-21 23:55:53 +0800

    url部分已经完成

--- clauncher-api/styles/js.js
+++ clauncher-api/styles/js.js
@@ -2,8 +2,8 @@ function inUrl(){
 	var requestEn = document.getElementById('requestEn');
 	var requestIn = document.getElementById('requestIn');
 	var requestSh = document.getElementById('requestSh');
-	pullPar();
-	var requestUrl = "http://"+requestEn.value+"api.c-launcher.com"+requestIn.value+urlPer;
+	var urlper = pullPar();
+	var requestUrl = "http://"+requestEn.value+"api.c-launcher.com"+requestIn.value+urlper;
 	requestSh.innerHTML = requestUrl;
 	return requestUrl;
 }
@@ -24,26 +24,40 @@ function settingOut(){
 	setting.style.display = "none";
 }
 function checkBoxClick(){
-    var parametersName = document.getElementsByClassName("parametersName");
-    var checkBox = document.getElementsByClassName("checkBox");
-    for(var i = 0 ;i<checkBox.length;i++){
-    	var checkBoxInput = checkBox[i].getElementsByTagName("input");
-    	console.log(checkBoxInput);
-		Event.add(checkBoxInput[0],"click",checkBoxInputClick);
-		function checkBoxInputClick(){
-			if(parametersName[i].value===0){
-				parametersName[i].value=1;
-			}else{
-				parametersName[i].value=0;
-			}
-		}
+    var parametersName = setting.getElementsByTagName("div");
+    var checkBox = [];
+    var tmp = [];
+    for(var i = 0,len = parametersName.length;i < len;i++){
+    	if(parametersName[i].className === 'parametersName'){
+    		tmp.push(parametersName[i]);
+    	}
+    	if(parametersName[i].className === 'checkBox'){
+    		checkBox.push(parametersName[i]);
+    	}
     }
+    parametersName = tmp;
+    for(var i = 0 ;i<checkBox.length;i++){
+    	doing(i)
+    }
+    function doing(n){    	
+    	var checkBoxInput = checkBox[n].getElementsByTagName("input");
+		Event.add(checkBoxInput[0],"click",function(){
+			checkBoxInputClick(n);
+		});
+    }
+	function checkBoxInputClick(i){
+		if(parametersName[i].getAttribute('value')==0){
+			parametersName[i].setAttribute('value',1);
+		}else{
+			parametersName[i].setAttribute('value',0);
+		}
+	}
 	
 }
 function inSet(){
     var parametersName = document.getElementsByClassName("parametersName");
 	for(var i = 0 ;i<parametersName.length;i++){
-		if(parametersName[i].value){
+		if(parametersName[i].getAttribute('value') === "1"){
 			var date = parametersName[i].innerHTML;
 			addEl(date);
 		}
@@ -61,8 +75,9 @@ function pullPar(){
 	var input = parameters.getElementsByTagName("input");
 	var urlPer = "?";
 	for(var i=0;i<p.length;i++){
-		urlper = urlPer+p[i].innerHTML+"="+input[i].value+"&";
+		urlPer = urlPer+p[i].innerHTML+"="+input[i].value+"&";
 	}
+	return urlPer;
 }
 
 
