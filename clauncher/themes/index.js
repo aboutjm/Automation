de7c431bc5c2ee3749d3531dadd37047784ecb36
aboutjm <meimei0730@163.com>
2014-08-22 21:43:25 +0800

    拆分首页自动化测试的脚本

--- clauncher/themes/index.js
+++ clauncher/themes/index.js
@@ -4,10 +4,20 @@ casper.test.begin('Clauncher', 3, function suite(test) {
     });
     casper.then(function(){
     	this.capture('index.png');
+    });
+    casper.then(function(){
     	this.click('li.cyou-head-float-left a');
+    });
+    casper.then(function(){
         test.assertTitle("Clauncher -- Wonderful Android Launcher, Themes, Wallpapers, Ringstones and Homescreens", "home is ok");
-    	this.clickLabel(' Themes ','a');
+    });
+    casper.then(function(){	
+        this.clickLabel(' Themes ','a');
+    });
+    casper.then(function(){
         test.assertTitle("Download wonderful themes for your android phone -- Clauncher", "themes is ok");
+    });
+    casper.then(function(){
         this.capture('themes.png');
     });
     casper.back();
