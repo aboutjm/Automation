dc90849b881499a12310fcfa72bbdcda03224b2a
aboutjm <meimei0730@163.com>
2014-09-30 17:14:00 +0800

    自动化测试新建页签打开，尝试第二次

--- clauncher/themes/test.js
+++ clauncher/themes/test.js
@@ -1,3 +1,26 @@
+
+/*
+casper.waitForPopup(/popup\.html$/, function() {
+    this.test.assertEquals(this.popups.length, 1);
+});*/
+casper.test.begin('Clauncher', 1, function suite(test) {
+    casper.start("http://test.themes.c-launcher.com/", function() {
+        test.assertTitle("Clauncher -- Wonderful Android Launcher, Themes, Wallpapers, Ringstones and Homescreens", "home is ok");
+    });
+    casper.then(function() {
+        //casper.page.injectJs('jquery.js'); 
+        this.clickLabel(' Design Themes ', 'a');
+        this.wait(5000);
+        this.waitForPopup(/test.designer.c-launcher.com/, function(){
+            this.test.pass("popup opened");
+            this.capture("screen.png"); 
+        });
+    });
+    casper.run(function() {
+        test.done();
+    });
+});
+/*尝试的第一个*//*
 casper.test.begin('Clauncher', 1, function suite(test) {
     casper.start("http://test.themes.c-launcher.com/", function() {
         test.assertTitle("Clauncher -- Wonderful Android Launcher, Themes, Wallpapers, Ringstones and Homescreens", "home is ok");
@@ -17,4 +40,4 @@ casper.test.begin('Clauncher', 1, function suite(test) {
     casper.run(function() {
         test.done();
     });
-});
+});*/
