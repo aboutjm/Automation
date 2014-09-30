bed1fc7710111bc7e048313e63a3459336ab435b
aboutjm <meimei0730@163.com>
2014-09-30 17:23:18 +0800

    自动化测试新建页签打开，尝试第三次

--- clauncher/themes/test.js
+++ clauncher/themes/test.js
@@ -1,8 +1,21 @@
-
-/*
-casper.waitForPopup(/popup\.html$/, function() {
-    this.test.assertEquals(this.popups.length, 1);
-});*/
+casper.test.begin('Clauncher', 2, function suite(test) {
+    casper.start("http://test.themes.c-launcher.com/", function() {
+        test.assertTitle("Clauncher -- Wonderful Android Launcher, Themes, Wallpapers, Ringstones and Homescreens", "home is ok");
+    });
+    casper.then(function(){
+        this.clickLabel(' Design Themes ', 'a');
+    });
+    casper.waitForPopup(/test.designer.c-launcher.com/, function() {
+        this.test.assertEquals(this.popups.length, 1);
+    });
+    casper.then(function(){
+        this.capture("screen3.png"); 
+    });
+    casper.run(function() {
+        test.done();
+    });
+});
+/*尝试的第二个*//*
 casper.test.begin('Clauncher', 1, function suite(test) {
     casper.start("http://test.themes.c-launcher.com/", function() {
         test.assertTitle("Clauncher -- Wonderful Android Launcher, Themes, Wallpapers, Ringstones and Homescreens", "home is ok");
@@ -13,13 +26,13 @@ casper.test.begin('Clauncher', 1, function suite(test) {
         this.wait(5000);
         this.waitForPopup(/test.designer.c-launcher.com/, function(){
             this.test.pass("popup opened");
-            this.capture("screen.png"); 
+            this.capture("screen2.png"); 
         });
     });
     casper.run(function() {
         test.done();
     });
-});
+});*/
 /*尝试的第一个*//*
 casper.test.begin('Clauncher', 1, function suite(test) {
     casper.start("http://test.themes.c-launcher.com/", function() {
@@ -35,7 +48,7 @@ casper.test.begin('Clauncher', 1, function suite(test) {
     });
     casper.then(function() {
         this.wait(5000);
-        this.capture("screen.png"); 
+        this.capture("screen1.png"); 
     });
     casper.run(function() {
         test.done();
