33fb03afd492fbaf7df0d0a0843094b8e9b072f8
zymeimei <meimei0730@163.com>
2015-03-11 16:29:06 +0800

    bug修复

--- study/PracticeTyping/styles/js.js
+++ study/PracticeTyping/styles/js.js
@@ -1,21 +1,34 @@
 function check(){
+	console.log(practice);
 	var rText=reference.innerText;
 	var pText=practice.innerText;
-	var rTextA,pTextA;
+	console.log(practice);
+	var rTextA=[];
+	var pTextA=[];
+	console.log(pText,pTextA,practice);
 	for(i=0,ih=rText.length;i<ih;i++){
 		rTextA[i]=rText.substring(i,i+1);
 	}
 	for(j=0,jh=pText.length;j<jh;j++){
 		pTextA[j]=pText.substring(j,j+1);
+		//console.log(pText[j]);
 	}
-	for(x=0,xh=pText.length;x<xh;x++){
-		if(rTextA[x]!=pTextA[x]){
-			pTextA[x]="<font color='red'>"+pTextA[x]+"</font>";
+	console.log(pText,pTextA,practice);
+	//if(rTextA.length>1){
+		//start();
+	//}
+	//function start(){
+		for(x=0,xh=pTextA.length;x<xh;x++){
+			if(rTextA[x]!=pTextA[x]){
+				pTextA[x]="<font color='red'>"+pTextA[x]+"</font>";
+			}
 		}
-	}
-	var oText;
-	for(y=0,yh=pTextA.length;y<yh;y++){
-		oText+=pTextA[y];
-	}
-	practice.innerHTML=oText;
+		console.log(pText,pTextA,practice);
+		var oText="";
+		for(y=0,yh=pTextA.length;y<yh;y++){
+			oText+=pTextA[y];
+		}
+		console.log(oText,practice);
+		practice.innerHTML=oText;
+	//}
 }
